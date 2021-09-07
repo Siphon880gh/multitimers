7fd2b264ba569a7a57a9a62def30773993dc57a5
Weng Fei Fung <weffung@ucdavis.edu>
2021-09-07 01:20:21 -0700

    Better description and other UX

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- jQuery and Bootstrap  -->
     <script src="assets/js/vendors/HackTimer.min.js"></script>
     <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <link rel="stylesheet" href="assets/css/index.css">
 
     <!-- Articulate JS -->
@@ -20,9 +20,16 @@
 </head>
     <body>
         <div class="container">
-            <h3 id="title">Multiple Timers (Non-obtrusive)</h3>
-            <div id="desc-1"><small><b>By Weng Fei Fung</b>. <a class="fa fa-question" href="javascript:void();" onclick="help()"></a></small></div>
-            <div class="hide" id="desc-2"><p><small id="desc"><b>By Weng Fei Fung</b>. You can have as many alarms as you want. These alarms are designed to be non-obtrusive so that they do not continually beep until you're forced to interact with the alarm. An alarm will beep only once and stay in the background with the timer turning red. Once you address the alarm by stopping or restarting the timer, the red color disappears. So you are not interrupted with your tasks, yet there's a short audio and an obvious visual cue that an alarm is reached. You can name each alarm so you are reminded what that alarm is for.</p><p>There are other ways to use this tool. You can click a tap counter to increase its number if the activity makes sense to have a counting part, such as exercising when you do a workout set followed by a rest period. You can have the tap counter automatically count up and restart the timer each time the time is reached, when you turn on loop mode <span class="fa fa-history"></span>, such as for casual gaming where you have timed tasks that can be repeated. Please note, this window must be on the screen for a beep and red color, so you may want to resize the window to some corner of your screen.</small> <a class="fa fa-question" href="javascript:void();" onclick="help()"></a></p></div>
+            <h1 id="title">Multiple Timers (Non-obtrusive)</h1>
+            <div id="desc">
+                <div class="author-line">
+                    <span>By Weng Fei Fung</span>
+                    <a href="https://www.linkedin.com/in/weng-fung/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/Siphon880gh/" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="https://www.youtube.com/channel/UCg1O9uttSv3ZBzd1iep25Ig/" target="_blank"><i class="fab fa-youtube"></i></a>
+                </div>
+
+                <p>Add as many countup timers as you want. You can turn the countup into an alarm. You can use a tap counter with each timer. Name the timers. Put the tab away in the background and it will still work. <a href="javascript:void(0);" onclick="$(this).next().toggleClass('hide');"><i class="fa fa-question"></i></a><span class="hide"> I coded the alarms to be non-obtrusive so that they do not continually beep until you're forced to interact with the alarm. A timer will stay red to let you know the alarm has elapsed. If you find that it's too non-obtrusive, you can have a timer beep multiple times, then announce a word or sentence.</span></p></div>
 
             <hr id="main-hr" />
             <div class="spacer-v"></div>
@@ -36,7 +43,8 @@
 
             <template>
                 <div data-uid="__uid__" class="time-tile timer" data-o data-init style="position:relative;">
-                    <h4 class="title" contenteditable onblur="timers.updateTitle('__uid__', event)" onclick='document.execCommand("", false, null); if($(this).find(".fa-edit").length) $(this).text("");'>Title <span class="fa fa-edit"></span></h4>
+                    <div class="section-header">Timer</div>
+                    <div class="title" contenteditable onblur="timers.updateTitle('__uid__', event)" onclick='document.execCommand("", false, null); if($(this).find(".fa-edit").length) $(this).text("");'>Title <span class="fa fa-edit"></span></div>
                     <button class="remove-timer" data-role="none" type="button" onclick="timers.remove('__uid__', event);"><span class="fa fa-close"></button>
                     <table>
                         <tbody>
@@ -51,23 +59,43 @@
                                 <span class="fa fa-bell hoverable" onclick='let $alarmSetter = $(this).prev(".target"); shorthandSetAlarm($alarmSetter);' style="font-size:1.75ch; vertical-align:top;"></span>
                             </td>
                         </tr>
+                        <tr class="alarm-container">
+                            <td colspan="2">
+                                <span>Beep x</span>
+                                <select class="beeps-number">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr class="alarm-container">
+                            <td colspan="2">
+                                <span>Announce</span>
+                                <input class="announce" type="text" placeholder="Words"/>
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
-                    <div class="text-center">
 
+                    <div class="timer-controls">
                         <button class="play-timer hoverable" data-role="none" type="button" onclick="playPause(event);"><i class="fa fa-play"></i></button>
                         <button class="repeat-timer hoverable" data-role="none" type="button" onclick="here(event).reset(); here(event).start()"><span class="fa fa-backward"/></button>
                         <button class="loop hoverable" data-role="none" type="button" onclick="$(this).toggleClass('active'); if($(this).hasClass('active')) { if($(this).parent().find('.fa-play').length) $(this).parent().find('.play-timer').click(); }"><span class="fa fa-history"/></button>
-                        <hr/>
-                        <div class="tap-counter-wrapper">
-                            <span style="text-align:left; font-weight:550;">Tap Counter:</span><br/>
+                    </div>
+                    <hr/>
+
+                    <div class="tap-counter-wrapper">
+                        <div class="subsection-header">Tap Counter</div>
+                        <div class="tap-counter-num" onclick='incrementCount(here(event).$time)'>0</div>
+                        <div class="tap-counter-label" style="font-size:.75rem"></div>
+                        <div class="tap-counter-controls">
                             <span class="fa fa-backward icon" onclick='$(this).parent().find(".tap-counter-num").text(0);'></span>
                             <span class="fa fa-edit icon" onclick='setTapCounterLabels(event)'></span>
-                            <br/>
-                            <div class="tap-counter-num" onclick='incrementCount(here(event).$time)'>0</div>
-                            <div class="tap-counter-label" style="font-size:.75rem"></div>
                         </div>
                     </div>
+
                 </div>
             </template>
         </div> <!-- /.container -->
