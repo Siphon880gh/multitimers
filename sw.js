99dc6d8f1204a2e6031cc7b5500c457d2c0f7db5
Weng Fei Fung <weffung@ucdavis.edu>
2022-11-06 19:46:10 -0800

    Fixed Bug: Not consistently playing beep

--- sw.js
+++ sw.js
@@ -3,14 +3,13 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 
     event.waitUntil(
-        caches.open("precache-v3").then(cache => {
+        caches.open("precache-v4").then(cache => {
             // Caching path does not have to be preceded with `public/` because starting the path with `/`
             // will start off the path from wherever Express delivered the HTML route
             const filesToCache = [
                 "/",
                 "/tools/multitimers/index.html",
                 "/tools/multitimers/assets/css/index.css",
-                "/tools/multitimers/assets/js/app.js",
                 "/tools/multitimers/assets/js/beep.js",
                 "/tools/multitimers/assets/js/vendors/articulate.js/articulate.min.js",
                 "/tools/multitimers/assets/js/vendors/HackTimer.min.js",
